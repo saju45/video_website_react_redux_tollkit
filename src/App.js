9a69613839e9c615cb996bcb6d0da6137f447787
Shanawaj hossain saju <princesaju645@gmail.com>
2024-11-03 03:26:03 -0800

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home";
 import Video from "./pages/Video";
-// import Home from "./pages/Home";
 
 function App() {
-  return <Video />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
